068b23ea3271e68b3b190dc59fa34d1b79d39ce7
70UNIK <mc.etcher@mail.com>
2026-02-18 10:37:52 +1100

    fixed

--- data/overrides/entropy_recipes.lua
+++ data/overrides/entropy_recipes.lua
@@ -9,55 +9,66 @@ Entropy.EnhancementPoints.m_unik_bill = 5.3
 
 function UNIK.build_fixed_recipe(enhancements,result)
     local newTable = {}
+    local invalid = false
     for i,v in pairs(enhancements) do
+        if not Entropy.EnhancementPoints[i] then
+            invalid = true
+            break
+        end
         for j = 1, v do
             newTable[#newTable+1] = i
         end
     end
-    print(newTable)
-    if #newTable == 5 then
-        table.sort(newTable, function(a,b)return (enhancements[a])>(enhancements[b]) end)
+    --print(newTable)
+    if #newTable == 5 and not invalid then
+        table.sort(newTable, function(a,b)return (Entropy.EnhancementPoints[a])>(Entropy.EnhancementPoints[b]) end)
         local string = Entropy.ConcatStrings(newTable)
         Entropy.FixedRecipes[string] = result
-        print("Recipe " .. string .. " created successfully")
+        --print("Recipe " .. string .. " created successfully")
         return true
     else
-        print("INVALID RECIPE; MUST BE EXACTLY 5 CARDS!")
+        --print("INVALID RECIPE; MUST BE EXACTLY 5 CARDS!")
         return false
     end
 end
 
-UNIK.build_fixed_recipe({m_unik_timber = 5},'j_unik_beaver')
-UNIK.build_fixed_recipe({m_unik_pink = 5},'j_unik_reggie')
-UNIK.build_fixed_recipe({m_unik_dollar = 5},'j_unik_joker_dollar')
-UNIK.build_fixed_recipe({m_unik_namta = 5},'j_unik_earthmover')
-UNIK.build_fixed_recipe({m_unik_green = 5},'j_unik_onyx_agate')
-UNIK.build_fixed_recipe({m_unik_bill = 5},'j_mf_top10')
-UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 1, c_base = 3},'j_entr_tenner')
-UNIK.build_fixed_recipe({m_entr_flesh = 4,m_unik_namta = 1},'j_unik_monster_spawner')
-UNIK.build_fixed_recipe({m_unik_pink = 4,m_entr_disavowed = 1},'j_unik_xchips_hater')
-UNIK.build_fixed_recipe({m_entr_flesh = 4,m_entr_disavowed = 1},'j_unik_autocannibalism')
-UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 4},'j_unik_decaying_tooth')
-UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 3,m_unik_namta = 1},'j_unik_impounded')
-UNIK.build_fixed_recipe({m_bonus = 4,m_unik_pink = 1},'j_unik_chipzel')
-UNIK.build_fixed_recipe({c_base = 2,m_unik_pink = 3},'j_unik_uniku')
-UNIK.build_fixed_recipe({c_base = 4,m_unik_pink = 1},'j_unik_numerical_reinforcement')
-UNIK.build_fixed_recipe({m_steel = 2,m_lucky = 2, m_entr_disavowed = 1},'j_unik_megatron')
-UNIK.build_fixed_recipe({m_steel = 2,m_lucky = 2, c_base = 1},'j_unik_D16')
-UNIK.build_fixed_recipe({m_steel = 1,m_entr_flesh = 3,m_unik_pink = 1},'j_unik_lily_sprunki')
-UNIK.build_fixed_recipe({m_entr_ceramic = 1,m_entr_disavowed = 1,c_base = 3},'j_unik_vessel_kiln')
-UNIK.build_fixed_recipe({m_entr_prismatic = 2,m_unik_pink = 3},'j_unik_unik')
-UNIK.build_fixed_recipe({m_entr_prismatic = 2,m_glass = 3},'j_unik_white_lily_cookie')
-UNIK.build_fixed_recipe({m_entr_ethereal = 2,m_base = 3},'j_unik_ghost_joker')
-UNIK.build_fixed_recipe({m_unik_dollar = 3,m_gold = 2},'j_unik_compounding_interest')
-UNIK.build_fixed_recipe({m_entr_disavowed = 1, m_stone = 4},'j_unik_border_wall')
---bunco recipes
-UNIK.build_fixed_recipe({m_bunc_copper = 2,m_glass = 3},'j_unik_hall_of_mirrors')
-UNIK.build_fixed_recipe({m_bunc_copper = 5},'j_bunc_kite_experiment')
-UNIK.build_fixed_recipe({m_bunc_copper = 2,m_unik_pink = 1,c_base = 2},'j_unik_blossom')
-UNIK.build_fixed_recipe({m_bunc_copper = 1,m_lucky = 1,m_wild = 1, m_mult = 1, m_bonus = 1},'j_bunc_jmjb')
-UNIK.build_fixed_recipe({m_bunc_cracker = 5},'j_bunc_hardtack')
-UNIK.build_fixed_recipe({m_steel = 1, m_gold = 2, m_stone = 2},'j_bunc_dwaven')
+local initializer = Game.unik_initialize_stuff
+function Game:unik_initialize_stuff()
+    local ret = initializer(self)
+        UNIK.build_fixed_recipe({m_unik_timber = 5},'j_unik_beaver')
+        UNIK.build_fixed_recipe({m_unik_pink = 5},'j_unik_reggie')
+        UNIK.build_fixed_recipe({m_unik_dollar = 5},'j_unik_joker_dollar')
+        UNIK.build_fixed_recipe({m_unik_namta = 5},'j_unik_earthmover')
+        UNIK.build_fixed_recipe({m_unik_green = 5},'j_unik_onyx_agate')
+        UNIK.build_fixed_recipe({m_unik_bill = 5},'j_mf_top10')
+        UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 1, c_base = 3},'j_entr_tenner')
+        UNIK.build_fixed_recipe({m_entr_flesh = 4,m_unik_namta = 1},'j_unik_monster_spawner')
+        UNIK.build_fixed_recipe({m_unik_pink = 4,m_entr_disavowed = 1},'j_unik_xchips_hater')
+        UNIK.build_fixed_recipe({m_entr_flesh = 4,m_entr_disavowed = 1},'j_unik_autocannibalism')
+        UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 4},'j_unik_decaying_tooth')
+        UNIK.build_fixed_recipe({m_unik_bill = 1,m_entr_disavowed = 3,m_unik_namta = 1},'j_unik_impounded')
+        UNIK.build_fixed_recipe({m_bonus = 4,m_unik_pink = 1},'j_unik_chipzel')
+        UNIK.build_fixed_recipe({c_base = 2,m_unik_pink = 3},'j_unik_uniku')
+        UNIK.build_fixed_recipe({c_base = 4,m_unik_pink = 1},'j_unik_numerical_reinforcement')
+        UNIK.build_fixed_recipe({m_steel = 2,m_lucky = 2, m_entr_disavowed = 1},'j_unik_megatron')
+        UNIK.build_fixed_recipe({m_steel = 2,m_lucky = 2, c_base = 1},'j_unik_D16')
+        UNIK.build_fixed_recipe({m_steel = 1,m_entr_flesh = 3,m_unik_pink = 1},'j_unik_lily_sprunki')
+        UNIK.build_fixed_recipe({m_entr_ceramic = 1,m_entr_disavowed = 1,c_base = 3},'j_unik_vessel_kiln')
+        UNIK.build_fixed_recipe({m_entr_prismatic = 2,m_unik_pink = 3},'j_unik_unik')
+        UNIK.build_fixed_recipe({m_entr_prismatic = 2,m_glass = 3},'j_unik_white_lily_cookie')
+        UNIK.build_fixed_recipe({m_entr_ethereal = 2,m_base = 3},'j_unik_ghost_joker')
+        UNIK.build_fixed_recipe({m_unik_dollar = 3,m_gold = 2},'j_unik_compounding_interest')
+        UNIK.build_fixed_recipe({m_entr_disavowed = 1, m_stone = 4},'j_unik_border_wall')
+        --bunco recipes
+        UNIK.build_fixed_recipe({m_bunc_copper = 2,m_glass = 3},'j_unik_hall_of_mirrors')
+        UNIK.build_fixed_recipe({m_bunc_copper = 5},'j_bunc_kite_experiment')
+        UNIK.build_fixed_recipe({m_bunc_copper = 2,m_unik_pink = 1,c_base = 2},'j_unik_blossom')
+        UNIK.build_fixed_recipe({m_bunc_copper = 1,m_lucky = 1,m_wild = 1, m_mult = 1, m_bonus = 1},'j_bunc_jmjb')
+        UNIK.build_fixed_recipe({m_bunc_cracker = 5},'j_bunc_hardtack')
+        UNIK.build_fixed_recipe({m_steel = 1, m_gold = 2, m_stone = 2},'j_bunc_dwaven')
+    return ret
+end
+
 
 local entroper = Entropy.GetRecipeResult
 function Entropy.GetRecipeResult(val,jokerrares,seed)
